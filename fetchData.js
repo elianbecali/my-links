778cbf4828360c80e591927a2ed46b1ec3bed9cd
Elian Becali <elianbecaliaguiar@gmail.com>
2021-02-13 20:38:25 -0400

    feat: add get links

---
+++ fetchData.js
@@ -0,0 +1,15 @@
+const container = document.getElementById('container-links');
+
+(async () => {
+  const response = await fetch('data/links.json');
+  const data = await response.json();
+
+  data.map(link => {
+    if(link.isHighlight) {
+      container.innerHTML += `<li><a class='highlight' href='${link.url}'>${link.name}</a></li>`;
+      return;
+    }
+    container.innerHTML += `<li><a href='${link.url}'>${link.name}</a></li>`;
+    console.log(link.name, link.url);
+  })
+})();
